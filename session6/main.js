a37ecfc1284ba4070ebe0db3b358beb92d2e5a34
a1824jj <a1824jj@aiit.ac.jp>
2020-08-03 03:04:16 +0000

    js/basic - declaration variable

---
+++ session6/main.js
@@ -0,0 +1,20 @@
+//alert('hello');
+
+//var, let, const
+
+let name = 'Tom';
+console.log('hello' + name);
+
+// Number, String, Boolean,, Undefined, Null, Symbol
+
+//動的型付け言語
+let variable = 'str';
+variable = 12;
+console.log(typeof variable);
+variable = false;
+console.log(typeof variable);
+variable = undefined;
+console.log(typeof variable);
+
+//静的型付け言語
+//int num = 12;
