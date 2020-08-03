7ea221f8b9f3b55788d4d3bb344ef64c45c8d2a0
a1824jj <a1824jj@aiit.ac.jp>
2020-08-03 03:18:03 +0000

    js/basic - array

--- session6/main.js
+++ session6/main.js
@@ -35,10 +35,30 @@ const h = function (name, age) {
     return name + age;
 }
 h('code Mafia3', 30);
-*/
+
 
 let hello = 'hello world';
 console.log(hello.length);
 console.log(hello.toLocaleUpperCase());
 console.log(hello.slice(0,5));
 console.log(hello.split(' '));
+*/
+
+const arry = new Array(1,2,3,4,5,6, 'moji', false);
+console.log(arry);
+console.log(arry[3]);
+arry[5] = 8;
+console.log(arry[5]);
+console.log(arry.length);
+arry.push('new item');
+console.log(arry.length);
+console.log(arry);
+const p = arry.pop();
+console.log(p);
+console.log(arry);
+arry.unshift('add first item');
+console.log(arry);
+const val = arry.shift();
+console.log(val);
+console.log(arry);
+
