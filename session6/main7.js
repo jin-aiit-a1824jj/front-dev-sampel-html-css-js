23ff2695ee5b301dd154a25ecbbfb8e74730e29b
a1824jj <a1824jj@aiit.ac.jp>
2020-08-03 08:29:14 +0000

    js object / class

---
+++ session6/main7.js
@@ -0,0 +1,25 @@
+const obj = {
+    first_name: 'Mafia',
+    last_name: 'Code',
+    printFullName: function() {
+        console.log('hello');
+    }
+}
+
+console.log(obj.first_name);
+obj.printFullName();
+
+
+class MyObj {
+    constructor(){
+        this.first_name = 'Mafia';
+        this.last_name = 'Code';
+    }
+
+    printFullName() {
+        console.log('hello');
+    }
+}
+
+const obj2 = new MyObj();
+obj2.__proto__.printFullName();
