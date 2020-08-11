ea32f566f819dbe223a5aa526a700d1786f31128
a1824jj <a1824jj@aiit.ac.jp>
2020-08-11 02:30:00 +0000

    responsible excersice -17 refactor main.js 1

--- session8/scripts/main.js
+++ session8/scripts/main.js
@@ -1,17 +1,64 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const hero = new HeroSlider('.swiper-container');
-    hero.start();
+    // const hero = new HeroSlider('.swiper-container');
+    // hero.start();
 
-    const cb = function (el, isIntersecting) {
-        if(isIntersecting) {
-            const ta = new TweenTextAnimation(el);
-            ta.animate();
-        }
+    // const cb = function (el, isIntersecting) {
+    //     if(isIntersecting) {
+    //         const ta = new TweenTextAnimation(el);
+    //         ta.animate();
+    //     }
+    // }
+
+    // const so = new ScrollObserver('.tween-animate-title', cb);
+
+    // const _inviewAnimation = function(el, inview){
+    //     if(inview){
+    //         el.classList.add('inview');
+    //     }else{
+    //         el.classList.remove('inview')
+    //     }
+    // };
+
+    //const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
+
+    //const header = document.querySelector('.header');
+    // const _navAnimation = function(el, inview){
+    //     if(inview){
+    //         header.classList.remove('triggered');
+    //     }else{
+    //         header.classList.add('triggered')
+    //     }
+    // };
+
+    //const so3 = new ScrollObserver('.nav-trigger', _navAnimation, {once: false});
+    
+    
+
+    new Main();
+});
+
+class Main {
+    constructor(){
+        this.header = document.querySelector('.header');
+        this._observers = [];
+        this._init();
     }
 
-    const so = new ScrollObserver('.tween-animate-title', cb);
+    _init() {
+        new MobileMenu();
+        this.hero = new HeroSlider('.swiper-container');
+        this._scrollInit();
+    }
 
-    const _inviewAnimation = function(el, inview){
+    _navAnimation(el, inview){
+        if(inview){
+            this.header.classList.remove('triggered');
+        }else{
+            this.header.classList.add('triggered')
+        }
+    };
+
+    _inviewAnimation(el, inview){
         if(inview){
             el.classList.add('inview');
         }else{
@@ -19,19 +66,27 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     };
 
-    const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
-
-    const header = document.querySelector('.header');
-    const _navAnimation = function(el, inview){
-        if(inview){
-            header.classList.remove('triggered');
-        }else{
-            header.classList.add('triggered')
+    _textAnimation(el, isIntersecting) {
+        if(isIntersecting) {
+            const ta = new TweenTextAnimation(el);
+            ta.animate();
         }
-    };
+    }
 
-    const so3 = new ScrollObserver('.nav-trigger', _navAnimation, {once: false});
-    
-    new MobileMenu();
-});
+    _toggleSlideAnimation(el, inview){
+        if(inview){
+            this.hero.start();
+        }else{
+            this.hero.stop();
+        }
+    }
 
+    _scrollInit() {
+        this._observers.push(
+            new ScrollObserver('.tween-animate-title', this._textAnimation),
+            new ScrollObserver('.cover-slide', this._inviewAnimation),
+            new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false}),
+            new ScrollObserver('.swiper-container', this._toggleSlideAnimation.bind(this), {once: false}),
+        );
+    }
+}
